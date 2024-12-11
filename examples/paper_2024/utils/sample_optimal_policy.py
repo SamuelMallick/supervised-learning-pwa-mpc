4071ebf1e1ea167360ea5359e92df9446824cbc4
SamuelMallick <sam.mallick.97@gmail.com>
2024-12-11 13:26:23 +0100

    bring sampel script up with new agent

--- examples/paper_2024/utils/sample_optimal_policy.py
+++ examples/paper_2024/utils/sample_optimal_policy.py
@@ -15,30 +15,24 @@ np_random = np.random.default_rng(0)
 
 SAVE = True
 
-N = 5
+N = 11
 nx, nu = Model.nx, Model.nu
 system = Model.get_system()
 system_dict = Model.get_system_dict()
 
 mixed_integer_mpc = MixedIntegerMpc(system_dict, N, X_f=Model.X_f)
 time_varying_affine_mpc = TimeVaryingAffineMpc(system_dict, N, X_f=Model.X_f)
-agent = Agent(
-    system,
-    mixed_integer_mpc=mixed_integer_mpc,
-    time_varying_affine_mpc=time_varying_affine_mpc,
-    N=N,
-    learn_infeasible_regions=True,
-)
+agent = Agent(system, time_varying_affine_mpc=time_varying_affine_mpc, classifiers=[])
 
 action_mapper = PwaActionMapper(len(system.A), N)
 validation_samples = Model.sample_state_space(
-    d=0.5, np_random=np_random, sample_strategy="grid"
+    d=0.1, np_random=np_random, sample_strategy="grid"
 )
 valid_validation_states: list[np.ndarray] = []
 optimal_validation_actions: list[int] = []
 for idx, state in enumerate(validation_samples):
     print(f"evaluating optimal action for state {idx} of {len(validation_samples)}")
-    optimal_action = agent.get_switching_sequence_from_mpc(state)
+    optimal_action = agent.get_switching_sequence_from_mpc(state, mixed_integer_mpc)
     if optimal_action is not None:
         valid_validation_states.append(state)
         optimal_validation_actions.append(

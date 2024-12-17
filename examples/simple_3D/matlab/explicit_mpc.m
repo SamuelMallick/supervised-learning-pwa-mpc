5a4b0377f7255e9d1cde648939b4f18197dc54e1
SamuelMallick <sam.mallick.97@gmail.com>
2024-12-17 10:54:16 +0100

    explicit mpc for 3d ex

---
+++ examples/simple_3D/matlab/explicit_mpc.m
@@ -0,0 +1,65 @@
+clc 
+clear all
+
+% state constraints
+Ax = [1, 0, 0; -1, 0, 0; 0, 1, 0; 0, -1, 0; 0, 0, 1; 0, 0, -1];
+bx = [10; 10; 5; 5; 10; 10];
+
+% pwa regions 1
+A1 = [1, 0.5, 0.3; 0, 1, 1; 0, 0, 1];
+B1 = [0; 0.1; 1];
+c1 = [0; 0; 0];
+sys1 = LTISystem('A', A1, 'B', B1, 'f', c1);
+R1 = Polyhedron('A', [0, 1, 0; Ax], 'b', [1; bx]);
+sys1.setDomain('x', R1);
+
+A2 = [1, 0.2, 0.3; 0, 0.5, 1; 0, 0, 1];
+B2 = [0; 0.1; 1];
+c2 = [0.3; 0.5; 0];
+sys2 = LTISystem('A', A2, 'B', B2, 'f', c2);
+R2 = Polyhedron('A', [0, -1, 0; Ax], 'b', [-1; bx]);
+sys2.setDomain('x', R2);
+
+pwa_sys = PWASystem([sys1, sys2]);
+
+% constraints
+X = Polyhedron('A', Ax, 'b', bx);
+pwa_sys.x.with('setConstraint');
+pwa_sys.x.setConstraint = X;
+
+u_lim = 5;
+pwa_sys.u.min = -u_lim;
+pwa_sys.u.max = u_lim;
+
+% cost
+pwa_sys.x.penalty = OneNormFunction(diag([1, 1, 1]));
+pwa_sys.u.penalty = OneNormFunction(1);
+
+% % terminal ingredients
+% sys1.u.min = -u_lim;
+% sys1.u.max = u_lim;
+% sys1.x.penalty = OneNormFunction(diag([1, 1]));
+% sys1.u.penalty = OneNormFunction(1);
+% Tset = sys1.LQRSet;
+% Tcost = sys1.LQRPenalty;
+% A_ = [0.943554152340661, 0.126216752413879; -0.564458476593388, -0.737832475861210; 0.564458476593388, 0.737832475861210; 1, 0; -1, 0];
+% b_ = [1; 2; 2; 1; 6];
+% Tset = Polyhedron('A', A_, 'b', b_);
+% 
+% 
+% pwa_sys.x.with('terminalSet');
+% pwa_sys.x.terminalSet = Tset;
+
+times = [];
+num_partitions = [];
+for N = 3:5
+    mpc = MPCController(pwa_sys, N);
+    t0 = tic();
+    exp_mpc = mpc.toExplicit();
+    runtime = toc(t0);
+    nr = exp_mpc.nr;
+    num_partitions = [num_partitions, nr];
+    times = [times, runtime];
+    save("exp_mpc"+string(N)+".mat", "exp_mpc", "runtime", "nr")
+end
+
---
+++ examples/simple_3D/matlab/plot_partition.m
@@ -0,0 +1,3 @@
+o = load("exp_mpc"+string(5)+".mat");
+exp_mpc = o.exp_mpc;
+exp_mpc.partition.plot()
